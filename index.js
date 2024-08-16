983054e3028d7a2112d9637d41dc50af79394910
ClintDhan <dhaniellerono@gmail.com>
2024-08-16 13:23:24 +0800

    Fixed numbers that show as array

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
+// let showNumber = document.querySelector('.show');
+
+
+// for(let i = 0; i < document.querySelectorAll('.blue').length; i++ ) {
+
+//     document.querySelectorAll('.blue')[i].addEventListener("click", function() {
+    
+      
+      
+    
+//     })
+// }
 let showNumber = document.querySelector('.show');
-let num = [];
+Number(showNumber);
+document.querySelector('.seven').addEventListener("click", function(){
+    showNumber.innerHTML = showNumber.innerHTML + 7;
+})
+document.querySelector('.eight').addEventListener("click", function(){
+    showNumber.innerHTML = showNumber.innerHTML + 8;
+})
+document.querySelector('.nine').addEventListener("click", function(){
+    showNumber.innerHTML = showNumber.innerHTML + 9;
+})
 
+document.querySelector('.four').addEventListener("click", function(){
+    showNumber.innerHTML = showNumber.innerHTML + 4;
+})
+document.querySelector('.five').addEventListener("click", function(){
+    showNumber.innerHTML = showNumber.innerHTML + 5;
+})
+document.querySelector('.six').addEventListener("click", function(){
+    showNumber.innerHTML = showNumber.innerHTML + 6;
+})
 
-for(let i = 0; i < document.querySelectorAll('.blue').length; i++ ) {
-
-    document.querySelectorAll('.blue')[i].addEventListener("click", function() {
-         num.push(this.textContent);
-         showNumber.innerHTML = num;
-    })
-}
+document.querySelector('.three').addEventListener("click", function(){
+    showNumber.innerHTML = showNumber.innerHTML + 3;
+})
+document.querySelector('.two').addEventListener("click", function(){
+    showNumber.innerHTML = showNumber.innerHTML + 2;
+})
+document.querySelector('.one').addEventListener("click", function(){
+    showNumber.innerHTML = showNumber.innerHTML + 1;
+})
